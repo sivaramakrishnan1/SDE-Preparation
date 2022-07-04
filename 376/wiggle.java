59296e622379f6ffa4ab629f7756a92cfa75f3a1
Sivarama Krishnan Venkatrayan <sivageeven@gmail.com>
2022-07-04 22:30:40 +0530

    Day 1-Completed

--- 376/wiggle.java
+++ 376/wiggle.java
@@ -1,6 +1,5 @@
-
 /**
- * wiggle wiggle
+ * wiggle wiggle , for sure
  */
 
 import java.util.Scanner;
@@ -19,41 +18,35 @@ public class wiggle {
     }
 
     public static int doWiggle(int ar[]) {
-        if (ar.length <= 1) return ar.length; 
+        if (ar.length <= 1)
+            return ar.length;
 
         int count = 1, front = 0, back = 1;
         boolean vary = true;
 
         while (back < ar.length) {
-            if( count == 1 && ( ar[back] != ar[front]) )
-            {
+            if (count == 1 && (ar[back] != ar[front])) {
                 vary = (ar[back] > ar[front]);
             }
-            if (ar[back] > ar[front] && vary) 
-            {
+            if (ar[back] > ar[front] && vary) {
                 back++;
                 front++;
                 count++;
                 vary = !vary;
-            } 
-            else if (ar[back] <= ar[front] && vary) 
-            {
+            } else if (ar[back] <= ar[front] && vary) {
                 back++;
                 front++;
-            }
-            else if (ar[back] < ar[front] && !vary) 
-            {
+            } else if (ar[back] < ar[front] && !vary) {
                 back++;
                 front++;
                 count++;
                 vary = !vary;
-            } 
-            else 
-            {
+            } else {
                 front++;
                 back++;
             }
-            if (back == ar.length) break;
+            if (back == ar.length)
+                break;
         }
         return count;
     }
